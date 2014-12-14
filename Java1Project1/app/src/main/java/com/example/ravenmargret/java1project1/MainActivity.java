d7374a8b4195e0c64d1cd0140dcf3383c567aca2
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2014-12-13 21:23:39 -0500

    Trying to fix errors

--- Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
+++ Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
@@ -5,34 +5,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import java.util.ArrayList;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 
 
 public class MainActivity extends ActionBarActivity
 {
 
+    private  JButton button;
+    private  JTextField textBox;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public static void main()
-    {
-        ArrayList<String> arrayObject= new ArrayList<String>();
+        ArrayList<String> arrayObject = new ArrayList<String>();
         JButton button = new JButton();
-        JtextField textBox = new JtextField ();
+        JtextField textBox = new JtextField();
 
-        button.addActionListener(new ActionListener()
-        {
+        button.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 //inside your action listener:
                 String add_item_to_array = textBox.getText().trim();
                 arrayObject.add(add_item_to_array);
