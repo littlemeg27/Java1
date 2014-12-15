da19d179b0682df34cecfca5ee0b95a28c65c7b9
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2014-12-14 19:10:07 -0500

    Trying to fix the sim

--- Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
+++ Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
@@ -4,36 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.ArrayList;
+
+
+
 
 
 
 public class MainActivity extends ActionBarActivity
 {
 
-    private  JButton button;
-    private  JTextField textBox;
+    private Button button;
+    private EditText textBox;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> arrayObject = new ArrayList<String>();
-        JButton button = new JButton();
-        JtextField textBox = new JtextField();
 
-        button.addActionListener(new ActionListener() {
+        final ArrayList<String> arrayObject = new ArrayList<String>();
+        button = (Button)findViewById(R.id.button);
+        textBox = (EditText)findViewById(R.id.editText);
 
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void onClick(View v)
+            {
                 //inside your action listener:
-                String add_item_to_array = textBox.getText().trim();
-                arrayObject.add(add_item_to_array);
-
+                String addItem = textBox.getText().toString();
+                arrayObject.add(addItem);
 
             }
         });
+
     }
 
 
