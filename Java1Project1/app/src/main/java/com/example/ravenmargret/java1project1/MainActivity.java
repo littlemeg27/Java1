143f3380cb89f4875e9293a9aafda06ac6df40ae
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-01-11 20:50:06 -0500

    Got errors, not working

--- Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
+++ Java1Project1/app/src/main/java/com/example/ravenmargret/java1project1/MainActivity.java
@@ -11,12 +11,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-
-
-
+import static android.widget.EditText.*;
 
 
 public class MainActivity extends ActionBarActivity
@@ -26,13 +25,15 @@ public class MainActivity extends ActionBarActivity
     private EditText textBox;
     protected Button button2;
     private EditText textBox2;
+    private TextView textView;
+    private TextView textView2;
     Context mContext;
 
     int letterCount;
     int wordCount;
     int counter;
     int averageCount;
-    Editable indexNum;
+    int indexNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -65,6 +66,8 @@ public class MainActivity extends ActionBarActivity
         textBox = (EditText)findViewById(R.id.editText);
         button2 = (Button)findViewById(R.id.button2);
         textBox2 = (EditText)findViewById(R.id.editText2);
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
 
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -77,6 +80,7 @@ public class MainActivity extends ActionBarActivity
                 {
                     arrayObject.add(addItem); //Add item to the array list
                     Log.i("Title", addItem);
+                    textBox.setText(null);
 
                     letterCount = addItem.length(); //Find out the length word
                     Log.i("letter count", letterCount + "");
@@ -87,9 +91,12 @@ public class MainActivity extends ActionBarActivity
 
                 wordCount = arrayObject.size(); //Find the word count
                 Log.i("word count", wordCount + "");
+                textView.setText(String.valueOf(wordCount));
 
                 averageCount = counter / wordCount; //Find the average of the letters
                 Log.i("average count", averageCount + "");
+                textView2.setText(String.valueOf(averageCount));
+
             }
 
         }); //End of Button
@@ -103,8 +110,9 @@ public class MainActivity extends ActionBarActivity
                 //Inside the action listener
                 indexNum = textBox2.getText();
                 Log.i("index", arrayObject.get(indexNum));
+                textBox2.setText(null);
 
-                if()
+                if(!arrayObject.contains(addItem))
                 {
                     AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
                     dialog.setTitle("Picked an Index");
@@ -113,25 +121,33 @@ public class MainActivity extends ActionBarActivity
                     {
                         @Override
                         public void onClick(DialogInterface dialog, int which);
+
                         {
 
                         }
+
                     });
+                }
 
                 else
                 {
                     AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
                     dialog.setTitle("Picked an Index");
                     dialog.setMessage("The index you picked was invalid");
-                    dialog.setNegativeButton("Cancel", new DialogInterface dialog, int which);
+                    dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
                     {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which);
+                        {
 
-                    }
+                        }
+
+                    });
 
                 }
-            }
+            } //End of onClick
 
-        });
+        }); //End of second Button
 
 
 
