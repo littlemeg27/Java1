1d392346bdd538aa113d8dcdf82b07d93b631227
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-01-26 14:09:12 -0500

    changed to array list

--- Java1Project3/app/src/main/java/com/example/ravenmargret/java1project3/MainActivity.java
+++ Java1Project3/app/src/main/java/com/example/ravenmargret/java1project3/MainActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -26,7 +28,8 @@ public class MainActivity extends ActionBarActivity
     TextView temperament;
     TextView age;
     ListView listView;
-    HashMap <String, Dogs> dogs = new HashMap<String, Dogs>();
+    Spinner spinner;
+    ArrayList dogs;
 
 
     @Override
@@ -44,13 +47,14 @@ public class MainActivity extends ActionBarActivity
         temperament = (TextView)findViewById(R.id.temperament);
         age = (TextView)findViewById(R.id.age);
 
-        dogs.put("Golden Retriever", new Dogs("Golden Retriever", "Sporting","Medium Coat","Red & Gold","Reliable, Friendly, Kind","11 Years"));
-        dogs.put("Standard Poodle", new Dogs("Standard Poodle", "Non-Sporting","Wavy Coat","Various","Eager to please","12 Years"));
-        dogs.put("Greyhound", new Dogs("Greyhound", "Hound","Smooth Coat","Various","Even Tempered, Athletic, Gentle","15 Years"));
-        dogs.put("Great Dane", new Dogs("Great Dane", "Sporting","Smooth Coat","Various","Devoted, Reserved, Gentle","8 Years"));
-        dogs.put("Alaskan Malamute", new Dogs("Alaskan Malamute", "Working","Medium Coat","Black, Grey & White","Playful, Devoted, Loyal","13 to 16 years"));
-        dogs.put("Giant Schnauzer", new Dogs("Giant Schnauzer", "Working","Medium Coat","Black","Strong Willed, Loyal, Kind","10 Years"));
-        dogs.put("Airedale Terrier", new Dogs("Airedale Terrier", "Terrier","Wavy Coat","Brown and Black","Outgoing, Alert, Friendly","11.5 Years"));
+        dogs.add(new Dogs("Golden Retriever", "Sporting","Medium Coat","Red & Gold","Reliable, Friendly, Kind","11 Years"));
+        dogs.add(new Dogs("Standard Poodle", "Non-Sporting","Wavy Coat","Various","Eager to please","12 Years"));
+        dogs.add(new Dogs("Greyhound", "Hound","Smooth Coat","Various","Even Tempered, Athletic, Gentle","15 Years"));
+        dogs.add(new Dogs("Great Dane", "Sporting","Smooth Coat","Various","Devoted, Reserved, Gentle","8 Years"));
+        dogs.add(new Dogs("Alaskan Malamute", "Working","Medium Coat","Black, Grey & White","Playful, Devoted, Loyal","13 to 16 years"));
+        dogs.add(new Dogs("Giant Schnauzer", "Working","Medium Coat","Black","Strong Willed, Loyal, Kind","10 Years"));
+        dogs.add(new Dogs("Airedale Terrier", "Terrier","Wavy Coat","Brown and Black","Outgoing, Alert, Friendly","11.5 Years"));
     }
 
+    spinner
 }
