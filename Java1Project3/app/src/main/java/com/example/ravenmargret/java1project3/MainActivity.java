e8ccbeb121476c964d74a422fc452000a236ce68
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-01-26 23:46:37 -0500

    Working on spinner not working yet

--- Java1Project3/app/src/main/java/com/example/ravenmargret/java1project3/MainActivity.java
+++ Java1Project3/app/src/main/java/com/example/ravenmargret/java1project3/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -27,9 +29,10 @@ public class MainActivity extends ActionBarActivity
     TextView color;
     TextView temperament;
     TextView age;
-    ListView listView;
-    Spinner spinner;
+    //ListView listView;
+    Spinner theSpinner;
     ArrayList dogs;
+    ArrayAdapter<String> spinnerAdapter;
 
 
     @Override
@@ -47,14 +50,20 @@ public class MainActivity extends ActionBarActivity
         temperament = (TextView)findViewById(R.id.temperament);
         age = (TextView)findViewById(R.id.age);
 
-        dogs.add(new Dogs("Golden Retriever", "Sporting","Medium Coat","Red & Gold","Reliable, Friendly, Kind","11 Years"));
-        dogs.add(new Dogs("Standard Poodle", "Non-Sporting","Wavy Coat","Various","Eager to please","12 Years"));
+        dogs.add(new Dogs("Golden Retriever", "Sporting", "Medium Coat", "Red & Gold", "Reliable, Friendly, Kind", "11 Years"));
+        dogs.add(new Dogs("Standard Poodle", "Non-Sporting", "Wavy Coat", "Various", "Eager to please", "12 Years"));
         dogs.add(new Dogs("Greyhound", "Hound","Smooth Coat","Various","Even Tempered, Athletic, Gentle","15 Years"));
-        dogs.add(new Dogs("Great Dane", "Sporting","Smooth Coat","Various","Devoted, Reserved, Gentle","8 Years"));
-        dogs.add(new Dogs("Alaskan Malamute", "Working","Medium Coat","Black, Grey & White","Playful, Devoted, Loyal","13 to 16 years"));
-        dogs.add(new Dogs("Giant Schnauzer", "Working","Medium Coat","Black","Strong Willed, Loyal, Kind","10 Years"));
-        dogs.add(new Dogs("Airedale Terrier", "Terrier","Wavy Coat","Brown and Black","Outgoing, Alert, Friendly","11.5 Years"));
+        dogs.add(new Dogs("Great Dane", "Sporting", "Smooth Coat", "Various", "Devoted, Reserved, Gentle", "8 Years"));
+        dogs.add(new Dogs("Alaskan Malamute", "Working", "Medium Coat", "Black, Grey & White", "Playful, Devoted, Loyal", "13 to 16 years"));
+        dogs.add(new Dogs("Giant Schnauzer", "Working", "Medium Coat", "Black", "Strong Willed, Loyal, Kind", "10 Years"));
+        dogs.add(new Dogs("Airedale Terrier", "Terrier", "Wavy Coat", "Brown and Black", "Outgoing, Alert, Friendly", "11.5 Years"));
+
+        theSpinner = (Spinner) findViewById(R.id.spinner);
+        spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+
+        theSpinner.setAdapter(spinnerAdapter);
     }
 
-    spinner
+
+
 }
